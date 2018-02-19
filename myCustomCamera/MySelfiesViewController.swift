e97076293c644c27038e579239b04194bd9a9f52
Umberto <grimaldiumberto94@gmail.com>
2018-02-19 18:13:52 +0100

    changes

--- myCustomCamera/MySelfiesViewController.swift
+++ myCustomCamera/MySelfiesViewController.swift
@@ -19,57 +19,55 @@ class MySelfiesViewController: UITableViewController {
     
     var secondImageArray: [UIImage] = [#imageLiteral(resourceName: "Front Camera Icon"), #imageLiteral(resourceName: "flash"), #imageLiteral(resourceName: "close"),#imageLiteral(resourceName: "delete"), #imageLiteral(resourceName: "export"), #imageLiteral(resourceName: "export"), #imageLiteral(resourceName: "delete"), #imageLiteral(resourceName: "Video Camera Icon"), #imageLiteral(resourceName: "camera"), #imageLiteral(resourceName: "delete"), #imageLiteral(resourceName: "flash"), #imageLiteral(resourceName: "flash"), #imageLiteral(resourceName: "camera"), #imageLiteral(resourceName: "Video Camera Icon"), #imageLiteral(resourceName: "close"), #imageLiteral(resourceName: "close"), #imageLiteral(resourceName: "close")]
     
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         firstCollection.dataSource = self
         firstCollection.delegate = self
+        secondCollection.dataSource = self
+        secondCollection.delegate = self
         
          navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor(red:0.17, green:0.67, blue:0.71, alpha:1.0)]
     }
 
 
     //    MARK: UITableViewDatasource
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "myRow") as! FirstTableViewCell
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "myRow2") as! TableViewCell
-            return cell
-        }
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+//
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        if indexPath.section == 0 {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "myRow") as! FirstTableViewCell
+//            return cell
+//        } else {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "myRow2") as! TableViewCell
+//            return cell
+//        }
+//    }
     
     //    MARK: - MARK: UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.section == 0 {
             return 233
-        }else {
+        } else {
             return 600
         }
     }
     
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
+
         if section == 0 {
             let headerView = UIView()
             headerView.backgroundColor = UIColor.white
-            
+
            let headerLabel = UILabel()
             headerLabel.font = UIFont.systemFont(ofSize: 25, weight: .medium)
             headerLabel.textColor = UIColor(red:0.17, green:0.67, blue:0.71, alpha:1.0)
@@ -77,12 +75,12 @@ class MySelfiesViewController: UITableViewController {
             headerLabel.sizeToFit()
             headerView.addSubview(headerLabel)
             headerLabel.frame = headerLabel.frame.offsetBy(dx: 20, dy: 15)
-            
+
             return headerView
         } else {
             let headerView = UIView()
             headerView.backgroundColor = UIColor.white
-            
+
             let headerLabel = UILabel()
 
             headerLabel.font = UIFont.systemFont(ofSize: 25, weight: .medium)
@@ -90,34 +88,38 @@ class MySelfiesViewController: UITableViewController {
             headerLabel.text = "All"
             headerLabel.sizeToFit()
             headerView.addSubview(headerLabel)
-            headerLabel.frame = headerLabel.frame.offsetBy(dx: 20, dy: 15)
+            headerLabel.frame = headerLabel.frame.offsetBy(dx: 20, dy: 0)
             return headerView
         }
     }
     
-    
 }
+
 
 extension MySelfiesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView.tag == 1 {
+        if collectionView.tag == 1{
             return firstImageArray.count
         } else {
             return secondImageArray.count
         }
-        
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView.tag == 1 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            let cellImage = cell.viewWithTag(3) as! UIImageView
+            cellImage.image = firstImageArray[indexPath.row]
             return cell
         } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            let cellImage = cell.viewWithTag(4) as! UIImageView
+            cellImage.image = secondImageArray[indexPath.row]
             
             return cell
         }
     }
-
+    
 
 }
 
